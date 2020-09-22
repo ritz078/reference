426bb04de4bdcf7c87969191da4616d86a6159bf
ritz078 <rkritesh078@gmail.com>
2020-09-22 22:09:39 +0530

    fix pointerevent

--- src/utils/convertPointerToCoordinate.ts
+++ src/utils/convertPointerToCoordinate.ts
@@ -1,17 +1,14 @@
 export function convertPointerToCoordinate(
-  event: MouseEvent | TouchEvent,
+  event: PointerEvent,
   domElement: HTMLCanvasElement
 ) {
-  const SIDEBAR_WIDTH = window.visualViewport.width < 620 ? 0 : 290;
-
-  const clientX =
-    event instanceof TouchEvent ? event.touches[0].clientX : event.clientX;
-  const clientY =
-    event instanceof TouchEvent ? event.touches[0].clientY : event.clientY;
+  const SIDEBAR_WIDTH = window.screen.width < 620 ? 0 : 290;
 
   const x =
-    ((clientX - SIDEBAR_WIDTH) / domElement.parentElement.clientWidth) * 2 - 1;
-  const y = -(clientY / domElement.parentElement.clientHeight) * 2 + 1;
+    ((event.clientX - SIDEBAR_WIDTH) / domElement.parentElement.clientWidth) *
+      2 -
+    1;
+  const y = -(event.clientY / domElement.parentElement.clientHeight) * 2 + 1;
 
   return { x, y };
 }
