583d9251d7f2af801c3467f3ccc6e0a480512bb9
Ritesh Kumar <ritz078@users.noreply.github.com>
2023-10-02 01:28:52 +0530

    fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import dynamic from "next/dynamic";
-import { Settings } from "@components/Settings";
-import GitHubCorners from "react-github-corner";
 
 const Scene = dynamic(() => import("@components/ModelLoader/ModelLoader"), {
   ssr: false,
